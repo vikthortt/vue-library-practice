8f7c70887cfb376fa84622da9e15d8806cf609ed
Victor Torres <vikthort.t@gmail.com>
2021-06-23 00:46:34 -0500

    add named imports to definition file

--- library-example.d.ts
+++ library-example.d.ts
@@ -4,4 +4,6 @@ import Vue, { PluginFunction, VueConstructor } from 'vue';
 declare const LibraryExample: PluginFunction<any>;
 export default LibraryExample;
 
-export const LibraryExampleSample: VueConstructor<Vue>;
+export const Link: VueConstructor<Vue>;
+export const TextInput: VueConstructor<Vue>;
+export const NumberInput: VueConstructor<Vue>;
